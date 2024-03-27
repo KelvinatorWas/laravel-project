41a443ab59ead876532cbedfeac1abdd0601280d
KelvinatorWas <liepkalnskalvis@gmail.com>
2024-03-27 22:20:43 +0200

    refactoring

--- resources/views/components/job-tags.blade.php
+++ resources/views/components/job-tags.blade.php
@@ -1,9 +1,9 @@
-@props(['listing'])
+@props(['tags'])
 
 <ul class="flex">
-  @foreach (explode(', ', $listing->tags) as $tag)
+  @foreach (explode(', ', $tags) as $tag)
     <li class="mr-2 flex items-center justify-center rounded-xl bg-black px-4 py-1 text-sm text-white">
-      <a href="#">{{ $tag }}</a>
+      <a href="/?tag{{ $tag }}">{{ $tag }}</a>
     </li>
   @endforeach
 </ul>

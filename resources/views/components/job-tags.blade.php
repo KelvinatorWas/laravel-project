152035310a209a4455c51e36e8ca7ce9b747d97b
KelvinatorWas <liepkalnskalvis@gmail.com>
2024-03-27 22:17:11 +0200

    simple tag component, Takes in tags from database

---
+++ resources/views/components/job-tags.blade.php
@@ -0,0 +1,9 @@
+@props(['listing'])
+
+<ul class="flex">
+  @foreach (explode(', ', $listing->tags) as $tag)
+    <li class="mr-2 flex items-center justify-center rounded-xl bg-black px-4 py-1 text-sm text-white">
+      <a href="#">{{ $tag }}</a>
+    </li>
+  @endforeach
+</ul>

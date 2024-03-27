d23a5a486ac4963bde92ff4918b213706eeeedf1
KelvinatorWas <liepkalnskalvis@gmail.com>
2024-03-27 22:19:54 +0200

    now job post is a separate card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,28 @@
+@props(['listing'])
+
+<x-card> 
+  <div class="flex">
+    <img src="{{ asset('images/no-image.png') }}" alt="" class="mr-6 hidden h-48 w-48 self-center md:block" />
+    <div class="flex flex-col justify-center">
+      <h2 class="text-2xl hover:underline">
+        <a href="/listings/{{ $listing->id }}">
+          {{ $listing->title }}
+        </a>
+      </h2>
+
+      <div class="text-l mb-4 mt-1 font-semibold">
+        <i class="fa-solid fa-building"></i>
+        {{ $listing->company }}
+      </div>
+
+      <p class="mb-2">{{ $listing->description }}</p>
+
+      <x-job-tags :tags="$listing->tags"/>
+    
+      <div class="text-l mt-4">
+        <i class="fa-sharp fa-solid fa-location-dot"></i>
+        {{ $listing->location }}
+      </div>
+    </div>
+  </div>
+</x-card>

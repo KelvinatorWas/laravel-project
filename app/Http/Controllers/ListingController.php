834c24ca1bf568100303b25c52b8483fded87750
KelvinatorWas <liepkalnskalvis@gmail.com>
2024-03-28 19:16:54 +0200

    small changes

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,19 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
-    public function showAllListings() {
+    public function showAll() {
 
         return view('listings.show-all', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get(),
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6),
         ]);
     }
 
-    public function showOneListing(Listing $listing) {
+    public function showOne(Listing $listing) {
         return view('listings.show-one', [
             'listing' => $listing,
         ]);
+    }
+
+    public function create() {
+        return view('listings.create');
+    }
+
+    public function store(Request $request) {
+        $formFields = $request->validate(
+            [
+                'company' => ['required', Rule::unique('listings', 'company')],
+                'title' => 'required',
+                'location' => 'required',
+                'email' => ['required', 'email'],
+                'website' => 'required',
+                'tags' => 'required',
+                'description' => 'required',
+            ]
+        );
+
+        if ($request->hasFile("logo")) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        } 
+        
+        Listing::create($formFields);
+
+        return redirect('/')->with("success", "Listing created!");
     }
 }
